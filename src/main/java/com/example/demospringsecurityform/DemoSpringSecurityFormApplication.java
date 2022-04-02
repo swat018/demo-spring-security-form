6c42df731ecbb52314e438c526ad7c1366332c77
jinwoo park <swat018@gmail.com>
2022-04-02 12:37:18 +0900

    feat : 스프링 시큐리티 커스터마이징: PasswordEncoder

--- src/main/java/com/example/demospringsecurityform/DemoSpringSecurityFormApplication.java
+++ src/main/java/com/example/demospringsecurityform/DemoSpringSecurityFormApplication.java
@@ -2,9 +2,22 @@ package com.example.demospringsecurityform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoSpringSecurityFormApplication {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // 비추천
+//        return NoOpPasswordEncoder.getInstance();
+        // 추천
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(DemoSpringSecurityFormApplication.class, args);
--- src/test/java/com/example/demospringsecurityform/DemoSpringSecurityFormApplicationTests.java
+++ src/test/java/com/example/demospringsecurityform/DemoSpringSecurityFormApplicationTests.java
@@ -2,6 +2,9 @@ package com.example.demospringsecurityform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class DemoSpringSecurityFormApplicationTests {
