fb62d78827c1c30c8e9c154960a79a9d3c9ef29f
jinwoo park <swat018@gmail.com>
2022-04-05 14:12:41 +0900

    feat : 스프링 시큐리티 Filter 와 FilterChainProxy

--- src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
@@ -3,13 +3,16 @@ package com.example.demospringsecurityform.config;
 import com.example.demospringsecurityform.account.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
+@Order(Ordered.LOWEST_PRECEDENCE - 100)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
