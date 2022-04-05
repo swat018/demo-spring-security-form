66b9f5a7db7bcd443753e4572451e393c0069f33
jinwoo park <swat018@gmail.com>
2022-04-05 20:36:28 +0900

    feat : FilterSecurityInterceptor

--- src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
@@ -1,37 +1,17 @@
 package com.example.demospringsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDecisionManager;
-import org.springframework.security.access.AccessDecisionVoter;
 import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
-import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
-import org.springframework.security.web.access.expression.WebExpressionVoter;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-//    public AccessDecisionManager accessDecisionManager() {
-//        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
-//        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
-//
-//        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
-//        handler.setRoleHierarchy(roleHierarchy);
-//
-//        WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
-//        webExpressionVoter.setExpressionHandler(handler);
-//        List<AccessDecisionVoter<? extends Object>> voters = List.of(webExpressionVoter);
-//        return new AffirmativeBased(voters);
-//    }
 
     public SecurityExpressionHandler<FilterInvocation> expressionHandler() {
         RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
@@ -50,7 +30,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/admin").hasRole("ADMIN")
                 .mvcMatchers("/user").hasRole("USER")
                 .anyRequest().authenticated()
-//                .accessDecisionManager(accessDecisionManager())
                 .expressionHandler(expressionHandler())
                 .and()
             .formLogin()
