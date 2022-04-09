cedca7e70b08ad05035b1eadd23dd0a8450394ea
jinwoo park <swat018@gmail.com>
2022-04-10 08:51:23 +0900

    feat : Async 웹 MVC를 지원하는 필터 - WebAsyncManagerintegrationFilter

--- README.md
+++ README.md
@@ -24,4 +24,4 @@
   - 
   - 스프링 시큐리티 ignoring() 1부
   - 스프링 시큐리티 ignoring() 2부
-  
+  - Async 웹 MVC를 지원하는 필터: WebAsyncManagerintegrationFilter
--- src/main/java/com/example/demospringsecurityform/form/SampleController.java
+++ src/main/java/com/example/demospringsecurityform/form/SampleController.java
@@ -2,12 +2,16 @@ package com.example.demospringsecurityform.form;
 
 import com.example.demospringsecurityform.account.AccountContext;
 import com.example.demospringsecurityform.account.AccountRepository;
+import com.example.demospringsecurityform.common.SecurityLogger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
+import java.util.concurrent.Callable;
 
 @Controller
 public class SampleController {
@@ -51,6 +55,17 @@ public class SampleController {
     public String user(Model model, Principal principal) {
         model.addAttribute("message", "Hello User, " + principal.getName());
         return "user";
+    }
+
+    @GetMapping("/async-handler")
+    @ResponseBody
+    public Callable<String> asyncHandler() {
+        SecurityLogger.log("MVC");
+        return () -> {
+            SecurityLogger.log("Callable");
+            return "Async Handler";
+        };
+
     }
 
 }
